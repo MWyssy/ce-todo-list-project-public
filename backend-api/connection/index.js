3da4f6fc2eec468925cc4b1fd328924d4bc04528
Emily Bennett <emily.bennett@northcoders.com>
2023-06-08 17:31:15 +0100

    adding comment

--- backend-api/connection/index.js
+++ backend-api/connection/index.js
@@ -4,4 +4,6 @@ if (!process.env.PGDATABASE && !process.env.DATABASEURL) {
   throw new Error("No PGDATABASE or DATABASEURL configured");
 }
 
+// needs changing to account for DATABASEURL
+
 module.exports = new Pool();
